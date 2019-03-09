94a42686487f9662e8488f547108490afc74c91c
Getulio Sánchez <valentin2507@gmail.com>
2019-03-09 13:39:50 -0500

    Issue #3038819: Use Social Auth Network Base

--- src/Plugin/Network/AmazonAuth.php
+++ src/Plugin/Network/AmazonAuth.php
@@ -2,16 +2,11 @@
 
 namespace Drupal\social_auth_amazon\Plugin\Network;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Url;
-use Drupal\social_api\Plugin\NetworkBase;
 use Drupal\social_api\SocialApiException;
+use Drupal\social_auth\Plugin\Network\NetworkBase;
 use Drupal\social_auth_amazon\Settings\AmazonAuthSettings;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Luchianenco\OAuth2\Client\Provider\Amazon;
-use Drupal\Core\Site\Settings;
 
 /**
  * Defines a Network Plugin for Social Auth Amazon.
@@ -33,67 +28,6 @@ use Drupal\Core\Site\Settings;
 class AmazonAuth extends NetworkBase implements AmazonAuthInterface {
 
   /**
-   * The logger factory.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelFactory
-   */
-  protected $loggerFactory;
-
-  /**
-   * The site settings.
-   *
-   * @var \Drupal\Core\Site\Settings
-   */
-  protected $siteSettings;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('config.factory'),
-      $container->get('logger.factory'),
-      $container->get('settings')
-    );
-  }
-
-  /**
-   * AmazonAuth constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param array $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory object.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   The logger factory.
-   * @param \Drupal\Core\Site\Settings $settings
-   *   The site settings.
-   */
-  public function __construct(array $configuration,
-                              $plugin_id,
-                              array $plugin_definition,
-                              EntityTypeManagerInterface $entity_type_manager,
-                              ConfigFactoryInterface $config_factory,
-                              LoggerChannelFactoryInterface $logger_factory,
-                              Settings $settings) {
-
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
-
-    $this->loggerFactory = $logger_factory;
-    $this->siteSettings = $settings;
-  }
-
-  /**
    * Sets the underlying SDK library.
    *
    * @return \Luchianenco\OAuth2\Client\Provider\Amazon|false
@@ -106,7 +40,7 @@ class AmazonAuth extends NetworkBase implements AmazonAuthInterface {
 
     $class_name = '\Luchianenco\OAuth2\Client\Provider\Amazon';
     if (!class_exists($class_name)) {
-      throw new SocialApiException(sprintf('The Amazon Library for the league oAuth not found. Class: %s.', $class_name));
+      throw new SocialApiException(sprintf('The Amazon library for PHP League OAuth2 not found. Class: %s.', $class_name));
     }
 
     /* @var \Drupal\social_auth_amazon\Settings\AmazonAuthSettings $settings */
@@ -118,8 +52,6 @@ class AmazonAuth extends NetworkBase implements AmazonAuthInterface {
         'clientId' => $settings->getClientId(),
         'clientSecret' => $settings->getClientSecret(),
         'redirectUri' => Url::fromRoute('social_auth_amazon.callback')->setAbsolute()->toString(),
-        'accessType' => 'offline',
-        'verify' => FALSE,
       ];
 
       // Proxy configuration data for outward proxy.
@@ -153,6 +85,7 @@ class AmazonAuth extends NetworkBase implements AmazonAuthInterface {
       $this->loggerFactory
         ->get('social_auth_amazon')
         ->error('Define Client ID and Client Secret on module settings.');
+
       return FALSE;
     }
 
